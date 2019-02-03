ce4b43a03ba1080251f1753bd7c93fd008260cac
Connie Hilarides <conni_h@outlook.com>
2019-02-02 17:43:27 -0800

    Initial Commit

---
+++ FFXIVLauncher/resource.h
@@ -0,0 +1,29 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Resource.rc
+//
+#define IDD_LAUNCHERDLG                 103
+#define IDD_OTPENTRY                    107
+#define IDC_LAUNCHBTN                   1001
+#define IDC_CANCELBTN                   1002
+#define IDC_USERNAMEBOX                 1004
+#define IDC_USERNAMELABEL               1005
+#define IDC_PASSWORDLABEL               1006
+#define IDC_PASSWORDBOX                 1007
+#define IDC_GAMEVERLABEL                1010
+#define IDC_REMUSRBOX                   1013
+#define IDC_REMPWDBOX                   1014
+#define IDC_USEOTPBOX                   1017
+#define IDC_EDIT1                       1018
+#define IDC_OKBTN                       1020
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        109
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
