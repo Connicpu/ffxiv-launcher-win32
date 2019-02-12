36aaf2fd0ca5a13d55e6231873d87bcc877fbf6d
Tyrone Trevorrow <tyrone@sudeium.com>
2019-02-12 20:30:21 +1100

    style: use spaces, remove unused debugging line

--- FFXIVLauncher/Login.cpp
+++ FFXIVLauncher/Login.cpp
@@ -255,8 +255,6 @@ static LoginResult GetRealSID(const std::string & sid, std::string & result)
     auto hr = DoRequest(req, resp);
     if (FAILED(hr)) return LoginResult::NetworkError;
 
-	std::string utf8_result(resp.body.begin(), resp.body.end());
-
     if (resp.status == 409 || (resp.status == 200 && resp.body.size() > 0))
     {
         return LoginResult::UpdateRequired;
